2825a68035b8b9ce3bdcaf01082b2a7af777071c
Ohad Srur <ohadsrur@gmail.com>
2018-05-30 21:48:49 +1000

    Change local secret to environment, add more validation to registration, add read and update to AccountScanParameters

--- FlaskWebProject/app.py
+++ FlaskWebProject/app.py
@@ -4,21 +4,28 @@ It contains the definition of routes and views for the application.
 """
 import os
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators, IntegerField
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, IntegerField, DecimalField, BooleanField
 from passlib.hash import sha256_crypt
 from functools import wraps
 from sqlConnection import get_sql_connection_string, get_all_sql_connection
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData, Table, select
 import pandas as pd
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
 
 app = Flask(__name__)
 
 # initialization
 app = Flask(__name__)
 
-app.secret_key='MySecretKey1@#$'
-app.config['SQLALCHEMY_DATABASE_URI'] = get_sql_connection_string(svr='svr',db='db',user='user',psw='password!')
+#Secrets from environment
+app.secret_key=os.environ.get('CC_APP_SECRET')
+CC_SVR = CC_DB = os.environ.get('CC_SVR')
+CC_USER = os.environ.get('CC_USER')
+CC_PSW = os.environ.get('CC_PSW')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = get_sql_connection_string(svr=CC_SVR,db=CC_DB,user=CC_USER,psw=CC_PSW)
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -52,15 +59,15 @@ class Account(db.Model):
     UserID = db.Column(db.Integer, primary_key=True)
     AccountID = db.Column(db.Integer, primary_key=True)
     AccountName = db.Column(db.String(50), unique=False, nullable=False)
-    StockCommission = db.Column(db.Float(precision='4,2'), unique=False, nullable=False)
-    StockCommissionPerSher = db.Column(db.Float(precision='3,3'), unique=False, nullable=True)
-    OptionCommission = db.Column(db.Float(precision='4,2'), unique=False, nullable=False)
-    OptionCommPerSher = db.Column(db.Float(precision='3,3'), unique=False, nullable=True)
-    TaxRate = db.Column(db.Float(precision='5,2'), unique=False, nullable=False)
-    USD = db.Column(db.Float(precision='9,2'), unique=False, nullable=False)
-    AUD = db.Column(db.Float(precision='9,2'), unique=False, nullable=False)
-    MinPerc = db.Column(db.Float(precision='5,5'), unique=False, nullable=True)
-    RequiredPerc = db.Column(db.Float(precision='5,5'), unique=False, nullable=True)
+    StockCommission = db.Column(db.Float(precision='4,2'),  nullable=False)
+    StockCommissionPerSher = db.Column(db.Float(precision='3,3'),  nullable=True)
+    OptionCommission = db.Column(db.Float(precision='4,2'),  nullable=False)
+    OptionCommPerSher = db.Column(db.Float(precision='3,3'),  nullable=True)
+    TaxRate = db.Column(db.Float(precision='5,2'),  nullable=False)
+    USD = db.Column(db.Float(precision='9,2'),  nullable=False)
+    AUD = db.Column(db.Float(precision='9,2'),  nullable=False)
+    MinPerc = db.Column(db.Float(precision='5,5'),  nullable=True)
+    RequiredPerc = db.Column(db.Float(precision='5,5'),  nullable=True)
 
     def __repr__(self):
         return '<Account %r>' % self.AccountID
@@ -69,14 +76,22 @@ class RegisterForm(Form):
     title = StringField('Title', [validators.Length(min=2, max=6)])
     firstName = StringField('First Name', [validators.Length(min=2, max=20)])
     surName = StringField('Surname', [validators.Length(min=2, max=20)])
-    username = StringField('Username', [validators.Length(min=4, max=30)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, ''numbers, dots or underscores')])
+    email = StringField('Email', validators=[Required(), Length(1, 64),Email()])
     phone = IntegerField('Phone')
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
-    confirm = PasswordField('Confirm Password')
+    confirm = PasswordField('Confirm Password', validators=[Required()])
+
+    def validate_email(self, field):
+        if UserAccount.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if UserAccount.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
 
 class StockPicks(db.Model):
     #Return Stock Pick Results
@@ -97,14 +112,58 @@ class StockPicks(db.Model):
     def __repr__(self):
         return '<StockPickScanResults %r>' % self.userID
 
+class AccountScanParameters(db.Model):
+    __tablename__ = 'AccountScanParameters'
+    AccountID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, primary_key=True)
+    PositionType = db.Column(db.String(10), primary_key=True)
+    WeeksNumForCalcProb = db.Column(db.Integer,   nullable=False)
+    MaxNumberOfShares = db.Column(db.Integer,   nullable=True)
+    MaxUSDInvestmentSizePerStock = db.Column(db.Integer,   nullable=True)
+    MaxExpiryWeeks = db.Column(db.Integer,  nullable=False)
+    MinCallProbability = db.Column(db.Float(precision='3,2'),  nullable=False)
+    MaxCallProbability = db.Column(db.Float(precision='3,2'),  nullable=False)
+    MinIncomeExPerc = db.Column(db.Float(precision='4,2'),  nullable=False)
+    MinIncomeNotExPerc = db.Column(db.Float(precision='4,2'),  nullable=False)
+    CheckDailyProbability = db.Column(db.Boolean)
+    MaxNumResultsPerExpiry = db.Column(db.Integer,   nullable=False)
+    MinTradingPrice = db.Column(db.Float(precision='4,2'),  nullable=False)
+    MaxTradingPrice = db.Column(db.Float(precision='4,2'),  nullable=True)
+    IncludePutResults = db.Column(db.Boolean)
+    ExcludeExpiryDuringEventDates = db.Column(db.Boolean)
+    ExcludeStocks = db.Column(db.String(2000), unique=False, nullable=True)
+    ExcludeIndustry = db.Column(db.String(2000), unique=False, nullable=True)
+    PutProtectionPercCheckPrice = db.Column(db.Float(precision='3,3'),  nullable=True)
+
+    def __repr__(self):
+        return '<AccountScanParameters %r>' % self.UserID
 # Update Form Class
-class UpdateForm(Form):
+class UpdateAccountDetails(Form):
     title = StringField('Title', [validators.Length(min=2, max=6)])
     firstName = StringField('First Name', [validators.Length(min=2, max=20)])
     surName = StringField('Surname', [validators.Length(min=2, max=20)])
     username = StringField('Username', [validators.Length(min=4, max=30)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
     phone = IntegerField('Phone')
+
+class UpdateAccountOptionParameters(Form):
+    WeeksNumForCalcProb = IntegerField('WeeksNumForCalcProb')
+    MaxNumberOfShares = IntegerField('MaxNumberOfShares')
+    MaxUSDInvestmentSizePerStock = IntegerField('MaxUSDInvestmentSizePerStock')
+    MaxExpiryWeeks = IntegerField('MaxExpiryWeeks')
+    MinCallProbability = DecimalField('MinCallProbability',places=2)
+    MaxCallProbability = DecimalField('MaxCallProbability',places=2)
+    MinIncomeExPerc = DecimalField('MinIncomeExPerc',places=2)
+    MinIncomeNotExPerc = DecimalField('MinIncomeNotExPerc',places=2)
+    CheckDailyProbability = BooleanField('CheckDailyProbability', false_values=None)
+    MaxNumResultsPerExpiry = IntegerField('MaxNumResultsPerExpiry')
+    MinTradingPrice = DecimalField('MinTradingPrice',places=2)
+    MaxTradingPrice = DecimalField('MaxTradingPrice',places=2)
+    IncludePutResults = BooleanField('IncludePutResults', false_values=None)
+    ExcludeExpiryDuringEventDates = BooleanField('ExcludeExpiryDuringEventDates', false_values=None)
+    ExcludeStocks = StringField('ExcludeStocks', [validators.Length(max=2000)])
+    ExcludeIndustry = StringField('ExcludeIndustry', [validators.Length(max=2000)])
+    PutProtectionPercCheckPrice = DecimalField('PutProtectionPercCheckPrice',places=3)
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
@@ -192,7 +251,7 @@ def logout():
 @is_logged_in
 def dailyResults():
     #Getting DB connection
-    connection_string, engine, connection = get_all_sql_connection(svr='svr',db='db',user='user',psw='password!')
+    connection_string, engine, connection = get_all_sql_connection(svr=CC_SVR,db=CC_DB,user=CC_USER,psw=CC_PSW)
     #Getting Account
     username = 'OhadS' #session['username']
     accountIDquery = "SELECT a.AccountID \
@@ -237,7 +296,7 @@ def myAccount():
 @is_logged_in
 def updateAccount():
     query = UserAccount.query.filter_by(username=session['username']).first()
-    form = UpdateForm(request.form)
+    form = UpdateAccountDetails(request.form)
 
     if request.method == 'POST' : #and form.validate()   
        # Update account
@@ -264,7 +323,7 @@ def updateAccount():
 @is_logged_in
 def parametersOptions():
 
-    query = AccountScanParameters.query.filter_by(username=session['username'], UserID='1').first()
+    query = AccountScanParameters.query.filter_by(UserID='1', PositionType='New').first()
 
     if query.UserID > 0:
         return render_template('parametersOptions.html', parametersOptions=query)
@@ -272,10 +331,44 @@ def parametersOptions():
         msg = 'No Account Found'
         return render_template('parameters.html', msg=msg)
     
-@app.route('/parametersOptions', methods=['GET', 'POST'])
+@app.route('/updateParameters', methods=['GET', 'POST'])
 @is_logged_in
 def updateParameters():
-    return 1
+    query = AccountScanParameters.query.filter_by(UserID='1', PositionType='New').first()
+    form = UpdateAccountOptionParameters(request.form)
+
+    if request.method == 'POST' : #and form.validate()   
+       # Update account
+        AccountScanParameters.query.filter_by(UserID='1', PositionType='New').update({
+            'WeeksNumForCalcProb': request.form['WeeksNumForCalcProb'],
+            'MaxNumberOfShares' : request.form['MaxNumberOfShares'],
+            'MaxUSDInvestmentSizePerStock' : request.form['MaxUSDInvestmentSizePerStock'],
+            'MaxExpiryWeeks' : request.form['MaxExpiryWeeks'],
+            'MinCallProbability' : request.form['MinCallProbability'],
+            'MaxCallProbability' : request.form['MaxCallProbability'],
+            'MinIncomeExPerc' : request.form['MinIncomeExPerc'],
+            'MinIncomeNotExPerc' : request.form['MinIncomeNotExPerc'],
+            #'CheckDailyProbability' : request.form['CheckDailyProbability'],
+            'MaxNumResultsPerExpiry' : request.form['MaxNumResultsPerExpiry'],
+            'MinTradingPrice' : request.form['MinTradingPrice'],
+            'MaxTradingPrice' : request.form['MaxTradingPrice'],
+            #'IncludePutResults' : request.form['IncludePutResults'],
+            #'ExcludeExpiryDuringEventDates' : request.form['ExcludeExpiryDuringEventDates'],
+            'ExcludeStocks' : request.form['ExcludeStocks'],
+            'ExcludeIndustry' : request.form['ExcludeIndustry'],
+            'PutProtectionPercCheckPrice' : request.form['PutProtectionPercCheckPrice'],
+        })
+
+        # Commit to DB
+        db.session.commit()
+
+        # Close connection
+        db.session.close()
+
+        flash('You successfully updated your Optionn Parameters', 'success')
+
+        return redirect(url_for('parametersOptions'))
+    return redirect(url_for('parametersOptions'))
 
 if __name__ == '__main__':
 
