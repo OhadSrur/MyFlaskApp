50ac86937084ad5d4b8f0f2b3bcdaa7aec5eb6b0
Ohad Srur <ohadsrur@gmail.com>
2018-08-05 21:35:02 +1000

    Add comment bubble to Stock ID, and remove columns to make more space.

--- FlaskWebProject/Web_App/tests/test_user_model.py
+++ FlaskWebProject/Web_App/tests/test_user_model.py
@@ -1,14 +1,19 @@
 import pytest
 from passlib.hash import sha256_crypt
+from Web_App.models import UserAccount
 
 
 def test_user_password(app_ctx,user):
-    psw = '$5$rounds=535000$SYi4G8EOI240Va4C$9YbgGlA.DSnCK693RQoULKAW5TnLUETaQlxxORUUw0B'
-    assert psw == user.password
-    #assert u.verify_password(psw)
+    assert user.verify_password('123456')
 
 def test_user_get_id(app_ctx,user):
     assert user.get_id() == 5
 
 def test_user_get_username(app_ctx,user):
     assert user.get_username() == 'jonedoe'
+
+def test_password_salts_are_random(app_ctx,user):
+    u = user.set_password('test')
+    u2 = user.set_password('test')
+    print (u.password)
+    assert u.password != u2.password
